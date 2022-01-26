17c055f443f855c4842bf269d9aaf4cf418f9f96
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2022-01-26 17:03:08 +0100

    Added some documentation

--- src/Cell.java
+++ src/Cell.java
@@ -2,9 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+/**
+ * This is a cell of the grid in the 15 game. It is an extension of JButton that holds an int value and requires an actionListener.
+ */
 public class Cell extends JButton {
+    /** the number value that the cell holds */
     private int value;
-    private Point cords;
+
     public Cell(int text, ActionListener handler){
         super();
         this.setNum(text);
@@ -12,15 +16,22 @@ public class Cell extends JButton {
         super.addActionListener(handler);
     }
 
-    public void setNum(int text) {
-        if(text <= 0){
+    /**
+     * Stores the number in the cell and displays it on the button. The button is empty when the number is below 1.
+     * @param num An int value to be displayed
+     */
+    public void setNum(int num) {
+        if(num <= 0){
             super.setText("");
         } else {
-            super.setText(String.valueOf(text));
+            super.setText(String.valueOf(num));
         }
-        value = text;
+        value = num;
     }
 
+    /**
+     * Returns the number value stored and displayed on the cell
+     */
     public int getNum() {
         return value;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.geom.Point2D;
-import java.util.Iterator;
 
 public class Main extends JFrame {
 
@@ -10,17 +7,24 @@ public class Main extends JFrame {
     public static final int height = width + 20;
     public static final int scale = 3;
     public static final String name = "15 Game";
+    /** coordinates of the empty space */
     private Point emptySpace = new Point(0, 0);
+    /** the matrix for all the cell buttons */
     private final Cell[][] matrix = new Cell[4][4];
+    /** the cheat counter */
     private int cheat = 0;
 
     public Main(){
         super(name);
+        // Set windows size and make it unresizable
         setMinimumSize(new Dimension(width * scale, height * scale));
         setMaximumSize(new Dimension(width * scale, height * scale));
         setPreferredSize(new Dimension(width * scale, height * scale));
+        setResizable(false);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set up the game-board and seed the cells
         var it = new GameGenerator(4).iterator();
         setLayout(new GridLayout(4,4,3,3));
 
@@ -29,27 +33,34 @@ public class Main extends JFrame {
             int num = it.next();
             int x = i % 4;
             int y = i / 4;
+
             if(num <= 0){
                 emptySpace = new Point(x, y);
             }
 
             matrix[x][y] = new Cell(num,
+                    // On click event handler
                     (e -> {
+                        // move value if it is adjacent (not diagonal) to the empty space
                         if ((Math.abs(x - emptySpace.x) == 1 && Math.abs(y - emptySpace.y) == 0) || (Math.abs(x - emptySpace.x) == 0 && Math.abs(y - emptySpace.y) == 1)){
                             matrix[emptySpace.x][emptySpace.y].setNum(matrix[x][y].getNum());
                             matrix[x][y].setNum(0);
                             emptySpace = new Point(x, y);
 
+                            // Display a message box when the game has been won
                             if(winConditionCheck()){
                                 JOptionPane.showMessageDialog(this, "Grattis! Du har vunnit!");
                             }
                         }
+                        // increment the cheat counter if the empty space is clicked
                         else if(emptySpace.x == x && emptySpace.y == y){
                             cheat++;
                             if(cheat == 10){
                                 commenceCheat();
                             }
+                            Toolkit.getDefaultToolkit().beep();
                         }
+                        // Beep when the wrong buttons are clicked
                         else {
                             Toolkit.getDefaultToolkit().beep();
                         }
@@ -57,14 +68,18 @@ public class Main extends JFrame {
                     );
 
             i++;
+            // Added cell butoon to the window grid
             add(matrix[x][y]);
         }
 
-        setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
+    /**
+     * Checks if the cell numbers are in order
+     * @return returns true when the cell numbers are in order
+     */
     private boolean winConditionCheck(){
         for (int i = 0; i < 15; i++){
             if(matrix[i % 4][i / 4].getNum() != (i + 1)){
@@ -74,6 +89,9 @@ public class Main extends JFrame {
         return true;
     }
 
+    /**
+     * When the empty space is clicked 10 times then set the game-board to an almost complete state
+     */
     private void commenceCheat(){
         for (int i = 0; i < 14; i++){
             matrix[i % 4][i / 4].setNum(i + 1);
@@ -83,6 +101,7 @@ public class Main extends JFrame {
         matrix[3][3].setNum(15);
     }
 
+    // Create the window
     public static void main(String[] args){
         new Main();
     }
